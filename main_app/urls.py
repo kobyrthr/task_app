6902bda86b3f3ee6371936d4a12a5944859773c8
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-15 20:47:40 -0400

    added TemplateView and tempalte inheritance to hom route

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+        path('', views.Home.as_view(), name="home"),
+        path('about/',views.About.as_view(), name="about")
+
 ]
