c660cd3fc5b1b94f7a08fb6a20970715dee85f5f
kobyrthr-dev <kobyrthr@gmail.com>
2022-04-15 23:57:28 -0400

    added seed data to create_tasks. now running into issues with postgres saying there are no relations found when trying to seed my taskapp db.

---
+++ db/create_tasks.sql
@@ -0,0 +1,8 @@
+INSERT INTO taskapp(name,priority,loe,status)
+VALUES("Complete Finch Collector homework","High","High","In progress");
+INSERT INTO taskapp(name,priority,loe,status)
+VALUES("Do the dishes","Medium","Low","Done");
+INSERT INTO taskapp(name,priority,loe,status)
+VALUES("Workout","High","Medium","To do");
+INSERT INTO taskapp(name,priority,loe,status)
+VALUES("Cook for the week","High","High","To do");
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+PRIORITY =  (
+    ("hp","high"),
+    ("mp","medium"),
+    ("lp","low")
+)
+
+LEVEL_OF_EFFORT =  (
+    ("he","high"),
+    ("me","medium"),
+    ("le","low")
+)
+
+STATUSES = (
+    ("0","To-do"),
+    ("1","Done")
+)
+
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    priority = models.CharField(max_length=25,choices = PRIORITY)
+    effort = models.CharField(max_length=25,choices = LEVEL_OF_EFFORT)
+    status = models.CharField(max_length=25,choices = STATUSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['status']
